bf8524c09e2b8ddbdded1c49f9ba036e79fb0d80
crazdrms <dilipmlove@gmail.com>
2017-01-18 18:51:45 +0545

    query param for pagination

--- src/main/java/com/dmaharjan/jaxrs/resources/MessageResource.java
+++ src/main/java/com/dmaharjan/jaxrs/resources/MessageResource.java
@@ -16,6 +16,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -29,7 +30,13 @@ public class MessageResource {
     private MessageService messageService = new MessageService();
 
     @GET
-    public List<Message> getMessages() {
+    public List<Message> getMessages(@QueryParam("year") int year, @QueryParam("start") int start, @QueryParam("size") int size) {
+        if (year > 0) {
+            return messageService.getAllMessagesForYear(year);
+        }
+        if (start >= 0 && size >= 0) {
+            return messageService.getAllMessagesPaginated(start, size);
+        }
         return messageService.getAllMessages();
     }
 
--- src/main/java/com/dmaharjan/jaxrs/service/MessageService.java
+++ src/main/java/com/dmaharjan/jaxrs/service/MessageService.java
@@ -8,6 +8,7 @@ package com.dmaharjan.jaxrs.service;
 import com.dmaharjan.jaxrs.database.ResourceDatabase;
 import com.dmaharjan.jaxrs.model.Message;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -20,12 +21,32 @@ public class MessageService {
     private Map<Long, Message> messages = ResourceDatabase.getMessages();
 
     public MessageService() {
-        messages.put(1L, new Message(1,"Hi there","Dilip"));
-         messages.put(2L, new Message(2,"Hi there as well","Philip"));
+        messages.put(1L, new Message(1, "Hi there", "Dilip"));
+        messages.put(2L, new Message(2, "Hi there as well", "Philip"));
     }
 
     public List<Message> getAllMessages() {
         return new ArrayList<Message>(messages.values());
+    }
+
+    public List<Message> getAllMessagesForYear(int year) {
+        List<Message> messagesForYear = new ArrayList<>();
+        Calendar calendar = Calendar.getInstance();
+        for (Message message : getAllMessages()) {
+            calendar.setTime(message.getCreated());
+            if (calendar.get(Calendar.YEAR) == year) {
+                messagesForYear.add(message);
+            }
+        }
+        return messagesForYear;
+    }
+
+    public List<Message> getAllMessagesPaginated(int start, int size) {
+        ArrayList<Message> messagesForPagination = new ArrayList<>(messages.values());
+        if (start + size > messagesForPagination.size()) {
+            return new ArrayList<Message>();
+        }
+        return messagesForPagination.subList(start, start + size);
     }
 
     public Message getMessage(long id) {
